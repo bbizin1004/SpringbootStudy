31b2b134ca77ebb59d057499d0f94b6390e2c394
bbizin1004 <bbizin1004@korea.com>
2024-04-05 18:20:17 +0900

    4.5일 커밋

---
+++ src/main/java/com/edu/springboot/MemberDTO.java
@@ -0,0 +1,30 @@
+package com.edu.springboot;
+
+
+public class MemberDTO {
+	private String id;
+	private String pass1;
+	private String name;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPass1() {
+		return pass1;
+	}
+	public void setPass1(String pass1) {
+		this.pass1 = pass1;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h2>Quiz - 회원인증폼 만들기</h2>
+	<ul>
+		<li><a href="/">Home</a></li>
+		<li><a href="./memberRegist.do">회원가입폼</a></li>
+		<li><a href="./memberLogin.do">로그인이요</a></li>
+	</ul>
+</body>
+</html>
